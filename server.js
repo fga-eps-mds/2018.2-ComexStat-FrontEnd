9ce4c6f9f996a774207497b578cc86449ef2d3cb
SannyaArvelos <sannyasantana@gmail.com>
2018-10-03 01:11:52 -0300

    Adding deployment settings.

---
+++ server.js
@@ -0,0 +1,17 @@
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/comexstat-app'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/comexstat-app/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
