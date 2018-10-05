32054f16a11c948431f01f4c17691afc8b860cd5
Rogério Júnior <junior.rogerio8@gmail.com>
2018-10-05 20:05:43 -0300

    Making first scratch
    Signed-off-by: Rogério Júnior <junior.rogerio8@gmail.com>
    Co-authored-by: João Victor <joao15victor08@gmail.com>
    Co-authored-by: Marcos Nery <marcosnery.comp@gmail.com>

---
+++ src/app/pages/searchpage/searchpage.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'searchpage',
+    templateUrl: './searchpage.component.html',
+    styleUrls: ['./searchpage.component.css']
+})
+
+export class SearchPageComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(){}
+
+}
