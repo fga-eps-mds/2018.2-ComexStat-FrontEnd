7448e3ccc99cf7cf887ac1534ab08aae0eeb73c7
Marcos Nery <marcosnery.comp@gmail.com>
2018-10-06 01:50:51 -0300

    Populating years dropdown-list of the searchpage
    Signed-off-by: Marcos Nery <marcosnery.comp@gmail.com>

--- src/app/pages/searchpage/searchpage.component.ts
+++ src/app/pages/searchpage/searchpage.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector: 'searchpage',
-    templateUrl: './searchpage.component.html',
-    styleUrls: ['./searchpage.component.css']
+  selector: "searchpage",
+  templateUrl: "./searchpage.component.html",
+  styleUrls: ["./searchpage.component.css"]
 })
-
 export class SearchPageComponent implements OnInit {
-    constructor() {}
+  years: String[];
 
-    ngOnInit(){}
+  constructor() {
+    this.years = ["2017"]; //initializing list so that the method "push" can be used
+    for (var ano = 2016; ano >= 1998; ano--) {
+      this.years.push(ano.toString()); //populating list "years" with the years between 1998 and 2016
+    }
+  }
 
+  ngOnInit() {}
 }
