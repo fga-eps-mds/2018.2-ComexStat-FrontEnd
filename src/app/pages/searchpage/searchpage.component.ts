a2b00b79e833c15c2af063cd96b6092934be828e
Marcos Nery <marcosnery.comp@gmail.com>
2018-10-11 14:55:17 -0300

    Connecting Angular to graphql endpoint by using Apollo package and sending test query
    Signed-off-by: Marcos Nery <marcosnery.comp@gmail.com>
    Co-authored-by:João Victor <joao15victor08@gmail.com>

--- src/app/pages/searchpage/searchpage.component.ts
+++ src/app/pages/searchpage/searchpage.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import gql from 'graphql-tag';
+
+import { AssetImportFacts, Query } from '../../types';
 
 @Component({
   selector: "searchpage",
@@ -7,13 +14,45 @@ import { Component, OnInit } from "@angular/core";
 })
 export class SearchPageComponent implements OnInit {
   years: String[];
+  importations: Observable<AssetImportFacts[]>;
 
-  constructor() {
+
+  constructor(private apollo: Apollo) {
     this.years = ["2017"]; //initializing list so that the method "push" can be used
     for (var ano = 2016; ano >= 1998; ano--) {
       this.years.push(ano.toString()); //populating list "years" with the years between 1998 and 2016
     }
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    //Sending query to GraphQL end-point and receiving its result
+    this.importations = this.apollo.watchQuery<Query>({
+      query: gql`
+      {
+        allImport{
+            edges{
+               node{
+                 name
+               }
+
+            }
+          
+        }
+        
+      }
+      `
+    })
+      .valueChanges
+      .pipe(
+        //Maping result objects to importations variable
+        map(result => result.data.allImport),
+      );
+    //Printing to console each element returned
+    this.importations.forEach(element => {
+      console.log(element);
+
+    });
+  }
+
 }
