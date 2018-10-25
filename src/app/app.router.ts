c91d8db66d81516d3c538e3f942686f8d4e836be
Kaique Borges <rique.kaique@gmail.com>
2018-10-24 22:35:08 -0300

    Implements result component that receives info from search and performs the query

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { SearchPageComponent } from './pages/searchpage/searchpage.component';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ResultPageComponent } from './pages/resultpage/resultpage.component';
 
 export const router: Routes = [
   { path: '', redirectTo:'homepage', pathMatch: 'full' },
   { path: 'homepage', component: HomepageComponent },
-  { path: 'search', component: SearchPageComponent }
+  { path: 'search', component: SearchPageComponent },
+  { path: 'resultpage', component: ResultPageComponent }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
